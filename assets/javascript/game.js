91a456563c9fda655980243e33886fb2d2b0a0bb
Rahul Vig <rahvee51395@gmail.com>
2019-01-31 17:59:03 -0500

    finished game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -42,7 +42,7 @@ var game = {
   keyPressed: function(event) {
     if (game.guesses.includes(event.key)) {
       game.guessesLeft++;
-    } else {
+    } else if (!game.random.includes(event.key)) {
       game.guesses.push(event.key);
       document.getElementById(
         "guessed-letters"
@@ -58,8 +58,73 @@ var game = {
       document.getElementById("text-field").textContent = game.blank.join(" ");
       if (!game.blank.includes("_ ")) {
         game.wins++;
-        document.getElementById("curr-pic").src = "./assets/images/lion.jpg";
-        document.getElementById("curr-pic").style.visibility = "visible";
+        if (game.random.includes("lion")) {
+          document.getElementById("curr-pic").src = "./assets/images/lion.jpg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("mouse")) {
+          document.getElementById("curr-pic").src = "./assets/images/mouse.jpg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("armadillo")) {
+          document.getElementById("curr-pic").src =
+            "./assets/images/armadillo.jpg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("bear")) {
+          document.getElementById("curr-pic").src = "./assets/images/bear.jpeg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("cat")) {
+          document.getElementById("curr-pic").src = "./assets/images/cat.jpg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("cobra")) {
+          document.getElementById("curr-pic").src = "./assets/images/cobra.jpg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("dog")) {
+          document.getElementById("curr-pic").src = "./assets/images/dog.jpg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("donkey")) {
+          document.getElementById("curr-pic").src =
+            "./assets/images/donkey.jpeg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("fox")) {
+          document.getElementById("curr-pic").src = "./assets/images/fox.jpeg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("giraffe")) {
+          document.getElementById("curr-pic").src =
+            "./assets/images/giraffe.jpeg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("human")) {
+          document.getElementById("curr-pic").src =
+            "./assets/images/human.jpeg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("monkey")) {
+          document.getElementById("curr-pic").src =
+            "./assets/images/monkey.jpg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("penguin")) {
+          document.getElementById("curr-pic").src =
+            "./assets/images/penguin.jpg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("tarantula")) {
+          document.getElementById("curr-pic").src =
+            "./assets/images/tarantula.jpg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
+        if (game.random.includes("zebra")) {
+          document.getElementById("curr-pic").src = "./assets/images/zebra.jpg";
+          document.getElementById("curr-pic").style.visibility = "visible";
+        }
         game.guesses = [];
         game.blank = [];
         game.guessesLeft = 12;
